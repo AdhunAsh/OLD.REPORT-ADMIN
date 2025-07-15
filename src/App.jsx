9791e54b1ef06a37c00bd433190c2f3e4f84463d
AdhunAsh <adhunav147@gmail.com>
2025-07-15 12:57:44 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
                             <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
                                 <Routes>
                                     <Route
-                                        path="/add"
+                                        path="/"
                                         element={<ProductForm />}
                                     />
                                     <Route
